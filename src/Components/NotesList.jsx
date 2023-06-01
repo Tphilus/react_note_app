a535903ed847e9bc2689bc4a58e8245c5b1251a3
Tphilus <tkdadzie@st.ug.edu.gh>
2023-06-01 04:22:00 +0000

    added components(Note, and notesList)

--- src/Components/NotesList.jsx
+++ src/Components/NotesList.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import Note from "./Note";
 
 const NotesList = () => {
-  return <div className="notes-list">{/* <Note /> */}</div>;
+  return (
+    <div className="notes-list">
+      <Note />
+      <Note />
+      <Note />
+      <Note />
+    </div>
+  );
 };
 
 export default NotesList;
